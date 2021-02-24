cba2369b539561b72b04db53ba9182ae77357d2b
Carsten <18394207+Aschratt@users.noreply.github.com>
2021-02-24 14:47:23 +0100

    Implemented matching and time measure.

--- src/Disparity/demo.cpp
+++ src/Disparity/demo.cpp
@@ -1,17 +1,28 @@
 #include "demo.hxx"
-#include <iostream>
 
 using namespace cv;
 using namespace cv::utils;
+
+#define STEREO_MATCHER_BM   0
+#define STEREO_MATCHER_SGBM 1
+
+String fileName(const String& absoluteName)
+{
+    auto separator = absoluteName.find_last_of('\\');
+
+    return separator == String::npos ? absoluteName : absoluteName.substr(separator + 1);
+}
 
 int main(int argc, char* argv[])
 {
     // Define command line arguments.
     const String keys =
         "{help h usage ? |      | Print this message. }"
-        "{left_dir dl    |      | Directory that contains the images of the left camera. Images must have the .png ending. }"
-        "{right_dir dr   |      | Directory that contains the images of the right camera. Images must have the .png ending. }"
-        "{matcher m      | 0    | Stereo matcher algorithm: 0 - binary BM (default), 1 - binary SGBM, 2 - quasi dense. }"
+        "{left_dir dl    |      | Directory that contains the images of the left camera. Images must have the .png ending. Corresponding images must have the same name. }"
+        "{right_dir dr   |      | Directory that contains the images of the right camera. Images must have the .png ending. Corresponding images must have the same name. }"
+        "{matcher m      | 0    | Stereo matcher algorithm: 0 - binary BM (default), 1 - binary SGBM. }"
+        "{disparities d  | 16   | Number of disparity levels. }"
+        "{block_size bs  | 9    | Size of a pixel kernel for comparison. }"
     ;
 
     // Parse command line arguments.
@@ -27,6 +38,8 @@ int main(int argc, char* argv[])
     String leftDir = fs::canonical(parser.get<String>("dl"));
     String rightDir = fs::canonical(parser.get<String>("dr"));
     int matcher = parser.get<int>("m");
+    int blockSize = parser.get<int>("bs");
+    int disparities = parser.get<int>("d");
 
     if (!parser.check())
     {
@@ -45,9 +58,9 @@ int main(int argc, char* argv[])
         std::cerr << "Directory does not exist: " << rightDir << std::endl;
         return 1;
     }
-    else if (matcher < 0 || matcher > 2)
+    else if (matcher != STEREO_MATCHER_BM && matcher != STEREO_MATCHER_SGBM)
     {
-        std::cerr << "Invalid matcher: " << matcher << ". Must be a value between 0 and 2. Use `help` for more info." << std::endl;
+        std::cerr << "Invalid matcher: " << matcher << ". Use `help` for more info." << std::endl;
         return 1;
     }
 
@@ -56,7 +69,58 @@ int main(int argc, char* argv[])
     fs::glob(leftDir, "*.png", leftImages);
     fs::glob(rightDir, "*.png", rightImages);
     
-    std::cout << "Found " << leftImages.size() << " images in left directory and " << rightImages.size() << " images in right directory." << std::cout;
+    std::cout << "Found " << leftImages.size() << " images in left directory and " << rightImages.size() << " images in right directory." << std::endl;
+
+    // Create stereo matcher instance.
+    Ptr<stereo::StereoMatcher> stereoMatcher;
+
+    switch (matcher)
+    {
+    case STEREO_MATCHER_BM:
+        stereoMatcher = stereo::StereoBinaryBM::create(disparities, blockSize);
+        break;
+    case STEREO_MATCHER_SGBM:
+        stereoMatcher = stereo::StereoBinarySGBM::create(0, disparities, blockSize);
+        break;
+    }
+
+    for (std::vector<String>::size_type i(0); i < leftImages.size(); ++i)
+    {
+        // Get the name of the last image.
+        String leftImage = leftImages[i];
+        String leftImageName = fileName(leftImage);
+        
+        // Find the corresponding right image.
+        auto match = std::find_if(rightImages.begin(), rightImages.end(), [&](const String& s) { return fileName(s) == leftImageName; });
+
+        if (match == rightImages.end())
+        {
+            std::cout << "Skipping image (no corresponding right image found): \"" << leftImage << "\"" << std::endl;
+            continue;
+        }
+
+        String rightImage = *match;
+        std::cout << "Interfering images \"" << leftImage << "\" and \"" << rightImage << "\"... ";
+
+        // Load images and match them.
+        Mat left = imread(leftImage, IMREAD_GRAYSCALE), right = imread(rightImage, IMREAD_GRAYSCALE);
+        
+        if (left.size() != right.size())
+        {
+            std::cout << std::endl << "Skipping image (image pair has not the same size): \"" << leftImage << "\"" << std::endl;
+            continue;
+        }
+
+        Mat disparity = Mat(left.size(), CV_8UC1);
+
+        auto start = std::chrono::high_resolution_clock::now();
+        stereoMatcher->compute(left, right, disparity);
+        
+        auto end = std::chrono::high_resolution_clock::now();
+        auto time = end - start;
+
+        std::cout << time / std::chrono::milliseconds(1) << "ms" << std::endl;
+    }
 
     return 0;
 }
--- src/Disparity/demo.hxx
+++ src/Disparity/demo.hxx
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
+#include <chrono>
 
 #include <opencv2/core.hpp>
 #include <opencv2/core/utility.hpp>
 #include <opencv2/core/utils/filesystem.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/stereo.hpp>
